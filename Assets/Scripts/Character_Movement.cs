629229824863ff32d217a359a478cb3cb26a833b
NathanT-W <nathtw1@gmail.com>
2019-11-29 11:25:00 +0000

    Added Player movement and temp camera movement
    Camera movement taken from online, will attempt to write own script at later date

---
+++ Assets/Scripts/Character_Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float gravity = 10.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CharacterController characterController = GetComponent<CharacterController>();
+
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+	}
+}
