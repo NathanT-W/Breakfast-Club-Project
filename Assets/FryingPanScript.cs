998502c71b8d672a36923bea61926bac424813c4
NathanT-W <nathtw1@gmail.com>
2020-02-27 03:49:39 +0000

    Cooking added

---
+++ Assets/FryingPanScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryingPanScript : MonoBehaviour {
+
+    public GameObject cookedBacon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider Col)
+    {
+        switch (Col.gameObject.tag)
+        {
+            case "Toast":
+
+                break;
+            case "Egg":
+
+                break;
+            case "Bacon":
+                Instantiate(cookedBacon, transform.position, transform.rotation);
+                Destroy(Col.gameObject);
+                break;
+        }
+    }
+}
