eef5f9bf4f208ddc1c634ff3e81cf13b71d00502
NathanT-W <nathtw1@gmail.com>
2020-03-24 16:58:53 +0000

    Added serving window script

---
+++ Assets/Scripts/serveFoodScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class serveFoodScript : MonoBehaviour {
+
+    void OnTriggerEnter(Collider Col)
+    {
+
+        if (Col.GetComponent<Plate_Food_Script>().food.egg == true)
+        {
+            Debug.Log(Col.GetComponent<Plate_Food_Script>().food.Egg.tag);
+        }
+        else
+        {
+            Debug.Log(0);
+        }
+
+        if (Col.GetComponent<Plate_Food_Script>().food.bacon == true)
+        {
+            Debug.Log(Col.GetComponent<Plate_Food_Script>().food.Bacon.tag);
+        }
+        else
+        {
+            Debug.Log(0);
+        }
+
+    }
+}
