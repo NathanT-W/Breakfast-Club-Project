4cde4d80c10cc3dad3da5c66a6b321215263e9fc
NathanT-W <nathtw1@gmail.com>
2020-03-25 21:12:52 +0000

    Revert "No"
    This reverts commit ce319cd9e77f665c0d49e0637e2ff67941d55b2a.

--- Assets/Scripts/serveFoodScript.cs
+++ Assets/Scripts/serveFoodScript.cs
@@ -25,9 +25,6 @@ public class serveFoodScript : MonoBehaviour {
             Debug.Log(0);
         }
 
-<<<<<<< HEAD
         Destroy(Col.gameObject);
-=======
->>>>>>> parent of 52596bb... Random bug fixes
     }
 }
