c36b3fb38c13d221ce52adeb806374fb6d2862fd
NathanT-W <nathtw1@gmail.com>
2020-07-04 22:51:09 +0100

    Made script for opening fridge and cupboard doors.

---
+++ Assets/Scripts/OpenDoorsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorsScript : MonoBehaviour {
+
+    HingeJoint doorHinge;
+    JointSpring doorSpring;
+
+    void OnMouseDown()
+    {
+        doorHinge = gameObject.GetComponent<HingeJoint>();
+        doorHinge.useSpring = true;
+        doorSpring = doorHinge.spring;
+        doorSpring.spring = 0.1f;
+        doorSpring.targetPosition = -100;
+        doorHinge.spring = doorSpring;
+    }
+}
