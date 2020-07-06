d8dffbf3c03e7239cd0f133a81d31f08ad69d863
NathanT-W <nathtw1@gmail.com>
2020-07-06 21:41:43 +0100

    Made it possible to make tea

--- Assets/Scripts/TeaScript.cs
+++ Assets/Scripts/TeaScript.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class TeaScript : MonoBehaviour {
 
-    void OnTriggerenter(Collider col)
+    public Mesh milkCupMesh;
+    public Material milkCup;
+    public Mesh finishedCupMesh;
+    public Material finishedCup;
+
+    void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Milk")
         {
             switch (gameObject.tag)
             {
                 case "EmptyCup":
+                    gameObject.GetComponent<MeshRenderer>().material = milkCup;
+                    gameObject.GetComponent<MeshFilter>().mesh = milkCupMesh;
+                    gameObject.tag = "MilkCup";
                     break;
                 case "EmptyMug":
                     break;
                 case "TeaMug":
                     break;
                 case "TeaCup":
+                    gameObject.GetComponent<MeshRenderer>().material = finishedCup;
+                    gameObject.GetComponent<MeshFilter>().mesh = finishedCupMesh;
+                    gameObject.tag = "FinishedCup";
                     break;
             }
         }
