b452885f26548660a9caab8365bf49767a28f577
NathanT-W <nathtw1@gmail.com>
2020-07-06 20:41:24 +0100

    More tea scripting

---
+++ Assets/Scripts/TeaScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeaScript : MonoBehaviour {
+
+    void OnTriggerenter(Collider col)
+    {
+        if (col.gameObject.tag == "Milk")
+        {
+            switch (gameObject.tag)
+            {
+                case "EmptyCup":
+                    break;
+                case "EmptyMug":
+                    break;
+                case "TeaMug":
+                    break;
+                case "TeaCup":
+                    break;
+            }
+        }
+
+    }
+
+
+}
