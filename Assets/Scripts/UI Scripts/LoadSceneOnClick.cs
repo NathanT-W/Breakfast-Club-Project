4c8b6b8215288cab9c1e35e9640434645e2f1e59
NathanT-W <nathtw1@gmail.com>
2020-07-15 15:10:33 +0100

    Fixed bug with transition to level 2

--- Assets/Scripts/UI Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/UI Scripts/LoadSceneOnClick.cs
@@ -7,7 +7,7 @@ public class LoadSceneOnClick : MonoBehaviour
 
     public void LoadByIndex(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
         Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneIndex);
     }
 }
